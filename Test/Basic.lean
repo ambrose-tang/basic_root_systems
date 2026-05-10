8add8e479a5f3d46f815d5af27d3760a6a2e1310
ambrose-tang <ambrose-tang@hotmail.com>
2026-05-10 11:25:18 +0800

    Initial Weyl Group structure

--- Test/Basic.lean
+++ Test/Basic.lean
@@ -6,11 +6,14 @@ open Submodule
 
 structure RootSystem (E : Type*) [NormedAddCommGroup E] [InnerProductSpace ℝ E] where
   Φ : Set E
-  σ : E -> E -> E := fun α β => β - ((2 * @inner ℝ E _ α β)/(@inner ℝ E _ α α)) • α
   coeff : E -> E -> ℝ := fun α β => ((2 * @inner ℝ E _ α β)/(@inner ℝ E _ α α))
+  σ : E -> E -> E := fun α β => β - coeff α β • α
+  W : Subgroup (Equiv.Perm E) :=
+    Subgroup.closure {(⟨σ α, σ α, fun β ↦ by sorry
+    , sorry⟩ : Equiv.Perm E) | α ∈ Φ}
 
   -- axioms
   R1 : Set.Finite Φ ∧ Submodule.span ℝ Φ = ⊤ ∧ 0 ∉ Φ
-  R2 : ∀ α ∈ Φ, (Submodule.span ℝ ({α} : Set E) : Set E) ∩ Φ = {-α, α}
+  R2 : ∀ α : E, α ∈ Φ -> (Submodule.span ℝ ({α} : Set E) : Set E) ∩ Φ = {-α, α}
   R3: ∀ α β : E, α ∈ Φ -> β ∈ Φ -> σ α β ∈ Φ
   R4 : ∀ α β : E, α ∈ Φ -> β ∈ Φ -> ∃n : ℤ, ↑n = coeff α β
